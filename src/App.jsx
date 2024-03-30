189b57ca0e3ffd8839f858ad8b87f420ec2baae6
millenniumSonule <anshumansonuley@gmail.com>
2024-03-30 23:18:02 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Header from './components/Header';
+
 function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
-  
+  const [data, setData] = useState([]);
 
-  return (  
+  const handleForm = (event) => {
+    event.preventDefault();
+    setData([...data, firstName]);
+
+  };
+  useEffect(() => {
+    // alert(data);
+  }, [data]); // This will alert whenever the 'data' state changes
+
+  const handleFChange = (event) => {
+    setFirstName(event.target.value);
+
+  };
+
+  const handleLChange = (event) => {
+    setLastName(event.target.value);
+
+  };
+
+  return (
     <div className="abc">
-        <Header/>
+      <form onSubmit={handleForm}>
+        <label htmlFor="fName">Enter First Name:</label>
+        <input
+          type="text"
+          placeholder="Enter your first name"
+          value={firstName}
+          onChange={handleFChange}
+        />
+        <br />
+        <label htmlFor='lName'> Enter Last Name: </label>
+        <input
+          type='text'
+          placeholder='Enter Your Last Name'
+          value={lastName}
+          onChange={handleLChange}
+        />
+        <br />
+
+        <button type="submit">Submit</button>
+
+      </form>
+
+      <p>OUTPUT :</p>
+      <ul>
+          {data.map((item,index)=>(
+              <li key={index}>{item}</li>
+          ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
